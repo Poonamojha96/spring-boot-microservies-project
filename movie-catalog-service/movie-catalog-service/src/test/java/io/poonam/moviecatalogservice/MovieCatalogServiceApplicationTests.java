a77e2c1c6992c3eea9f6025ce1e8091b0d20991d
Poonam Ojha <40633410+Poonamojha96@users.noreply.github.com>
2021-03-27 21:23:56 +0530

    Add files via upload

---
+++ movie-catalog-service/movie-catalog-service/src/test/java/io/poonam/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.poonam.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
